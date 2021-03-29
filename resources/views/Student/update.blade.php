a3e58d5111d4e4491848d81c2827ba2256b5ddd3
dewianaaryani <aryanidewianarahmat@gmail.com>
2021-03-29 15:24:57 +0700

    Update form Success

--- resources/views/Student/update.blade.php
+++ resources/views/Student/update.blade.php
@@ -31,6 +31,45 @@
                 <option value="Perempuan" {{$students->gender == 'Perempuan' ? 'selected="selected"' : ''}}>Perempuan</option>
             </select>
         </div>
-        
+        <div class="mb-3">
+            <label  class="form-label">Tempat Tangal Lahir</label>
+            <div class="row">
+                <div class="col-md-4">
+                <input  class="form-control" name="birth_place" value="{{$students->birth_place}}">    
+                </div>
+                <div class="col-md-4">
+                <input  class="form-control" name="birth_date" type="date" value="{{$students->birth_date}}">    
+                </div>
+            </div>        
+        </div>
+        <div class="mb-3">
+            <label  class="form-label">Alamat</label>
+            <textarea name="address" id="" cols="20" rows="10" class="form-control">{{$students->address}}</textarea>    
+        </div>
+        <div class="form-group">
+            <label for="">Asal Sekolah</label>
+            <input class="form-control" type="text" name="school" id="" value="{{$students->school}}">
+        </div>
+        <div class="form-group">
+            <label for="">Kelas</label>
+            <select name="grade" id="" class="form-control">
+                <option value="X (Sepuluh)" {{$students->grade == 'X (Sepuluh)' ? 'selected="selected"' : ''}}>X (Sepuluh)</option>
+                <option value="XI (Sebelas)" {{$students->grade == 'XI (Sebelas)' ? 'selected="selected"' : ''}}>XI (Sebelas)</option>
+                <option value="XII (Dua Belas)" {{$students->grade == 'XII (Dua Belas)' ? 'selected="selected"' : ''}}>XII (Dua Belas)</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="">Jurusan</label>
+            <select name="major" id="" class="form-control">
+                <option value="OTKP" {{$students->major == 'OTKP' ? 'selected="selected"' : ''}}>OTKP</option>
+                <option value="TBG" {{$students->major == 'TBG' ? 'selected="selected"' : ''}}>TBG</option>
+                <option value="RPL" {{$students->major == 'RPL' ? 'selected="selected"' : ''}}>RPL</option>
+                <option value="HTL" {{$students->major == 'HTL' ? 'selected="selected"' : ''}}>HTL</option>
+                <option value="TKJ" {{$students->major == 'TKJ' ? 'selected="selected"' : ''}}>TKJ</option>
+                <option value="MMD" {{$students->major == 'MMD' ? 'selected="selected"' : ''}}>MMD</option>
+                <option value="BDP" {{$students->major == 'BDP' ? 'selected="selected"' : ''}}>BDP</option>
+            </select>
+        </div>
+    <button type="submit" class="btn btn-success">Update</button>
     </form>
 @endsection
